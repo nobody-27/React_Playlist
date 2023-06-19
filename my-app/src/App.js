c4a32a8869e3e4dddcaecae88bb6532faca848a4
nobody-27 <nobodypal27@gmail.com>
2023-06-19 14:12:20 +0530

    add new funation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,17 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import About from './components/About';
+import { useState } from 'react';
 
 let name=".....not found";
 
 function App() {
+  const [mode, setMode] = useState('dark'); //whether dark mode is enabled.
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = 'grey'
+    }
+
+    else{
+        setMode('light')
+        document.body.style.backgroundColor = 'white'
+    }
+
+      
+  }
+
   return (
 
   <>
  
-    <Navbar title="Nobody" about_test="About...." />
+    <Navbar title="Nobody" about_test="About...." mode={mode} toggleMode={toggleMode} />
     <div className='container my-3'>
-        <Textform heading="Enter the text" /> 
+        <Textform heading="Enter the text"  mode={mode}/> 
+        {/* <About/> */}
     </div>
     
     </>
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -3,8 +3,7 @@ import React from 'react';
 
 export default function Navbar(props) {
     return (
-        <div>
-             <nav className="navbar navbar-expand-lg navbar-light bg-light">
+<nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <a className="navbar-brand" href="#">{props.title}</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
@@ -15,14 +14,16 @@ export default function Navbar(props) {
       <li className="nav-item active">
         <a className="nav-link" href="#">Home {props.about_test}</a>
       </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
-      </li>
-     
     </ul>
     
+
+<div className={`form-check form-switch text-${props.mode === "light" ? "dark": 'light'}`}>
+  <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+  <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+</div>
+
   </div>
 </nav>
-        </div>
+
     )
 }
