b4ba765ce38d666bbb19cdd9935433ce7f3b348d
nobody-27 <nobodypal27@gmail.com>
2023-07-12 15:06:17 +0530

    added new function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,36 +3,51 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import About from './components/About';
+import Alert from './components/Alert';
 import { useState } from 'react';
 
-let name=".....not found";
 
 function App() {
-  const [mode, setMode] = useState('dark'); //whether dark mode is enabled.
+  const [mode, setMode] = useState('light'); //whether dark mode is enabled.
+
+  const [alert, setalert] = useState(null); 
+
+  const showalert = (message,type) =>{
+    setalert(({
+      msg:message,
+      type: type
+    }))
+
+  }
+
+
+
 
   const toggleMode = () => {
     if(mode === 'light'){
       setMode('dark')
       document.body.style.backgroundColor = 'grey'
     }
-
     else{
         setMode('light')
         document.body.style.backgroundColor = 'white'
     }
-
-      
   }
 
   return (
-
   <>
- 
     <Navbar title="Nobody" about_test="About...." mode={mode} toggleMode={toggleMode} />
+    
+    <Alert  alertmessage={alert} ></Alert>
+
+
+
     <div className='container my-3'>
         <Textform heading="Enter the text"  mode={mode}/> 
-        {/* <About/> */}
     </div>
+
+
+    
     
     </>
   );
--- my-app/src/components/Textform.js
+++ my-app/src/components/Textform.js
@@ -20,16 +20,16 @@ export default function Textform(props){
     const [text, setText ] = useState("Enter text here");
     return (
         <>
-        <div className="container">
-             <h2 className="form-label">{props.heading} </h2>
+        <div className="container" style={{color: props.mode === 'dark'?'white':'black'}} >
+            <h2  className="form-label">{props.heading} </h2>
             <div className="mb-3">
-                <textarea  className="form-control" value={text} onChange={handleOnchange} id="mybox" rows="3"  />
+                <textarea  className="form-control" value={text} onChange={handleOnchange} style={{backgroundColor: props.mode === 'dark'?'grey':'white',color: props.mode === 'dark'?'white':'black'}} id="mybox" rows="3"  />
             </div>
             <button onClick={handleUpClick} className="btn btn-primary mx-2">Convert to Uppercase</button>
             <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
         </div>
 
-        <div className="container my-4">
+        <div className="container my-4" style={{color: props.mode === 'dark'?'grey':'black'}}>
             <h3>Your text summary</h3>
             <p>{text.split(" ").length} words, and {text.length} characters.. </p>
         </div>
